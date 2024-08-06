b6ac0ddcd218c78e07cea86182d2e0c6aa04bd9a
Trayana Boykova <trayana.boykova@gmail.com>
2024-08-06 11:18:25 +0300

    Update Contact.java

--- src/main/java/phonebook/entity/Contact.java
+++ src/main/java/phonebook/entity/Contact.java
@@ -1,4 +1,25 @@
 package phonebook.entity;
 
 public class Contact {
+    private String name;
+    private String number;
+
+    public Contact() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
 }
