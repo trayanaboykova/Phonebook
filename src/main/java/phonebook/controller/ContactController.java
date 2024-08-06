787c47e299a24bd8c9e48e96351b8a85135275a2
Trayana Boykova <trayana.boykova@gmail.com>
2024-08-06 11:31:47 +0300

    Update ContactController.java

--- src/main/java/phonebook/controller/ContactController.java
+++ src/main/java/phonebook/controller/ContactController.java
@@ -1,15 +1,26 @@
 package phonebook.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 import phonebook.entity.Contact;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class ContactController {
     private List<Contact> contacts;
 
-    public ContactController(List<Contact> contacts) {
-        this.contacts = contacts;
+    public ContactController() {
+        this.contacts = new ArrayList<>();
     }
+
+    @GetMapping("/")
+    public ModelAndView index(ModelAndView modelAndView) {
+        modelAndView.setViewName("index");
+        modelAndView.addObject("contacts", contacts);
+        return modelAndView;
+    }
+
 }
