9494c07d5d9071183c5589cb47468bd12b209a66
Trayana Boykova <trayana.boykova@gmail.com>
2024-08-06 11:37:41 +0300

    update

--- src/main/java/phonebook/controller/ContactController.java
+++ src/main/java/phonebook/controller/ContactController.java
@@ -2,6 +2,7 @@ package phonebook.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import phonebook.entity.Contact;
 
@@ -21,6 +22,12 @@ public class ContactController {
         modelAndView.setViewName("index");
         modelAndView.addObject("contacts", contacts);
         return modelAndView;
+    }
+
+    @PostMapping("/")
+    public String add (Contact contact) {
+        this.contacts.add(contact);
+        return "redirect:/";
     }
 
 }
