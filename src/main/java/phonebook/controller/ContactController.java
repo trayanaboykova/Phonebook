091f397755e89c95ab7b92145431d075fa8f08b7
Trayana Boykova <trayana.boykova@gmail.com>
2024-08-06 11:24:31 +0300

    Update ContactController.java

--- src/main/java/phonebook/controller/ContactController.java
+++ src/main/java/phonebook/controller/ContactController.java
@@ -1,8 +1,15 @@
 package phonebook.controller;
 
 import org.springframework.stereotype.Controller;
+import phonebook.entity.Contact;
+
+import java.util.List;
 
 @Controller
 public class ContactController {
-    
+    private List<Contact> contacts;
+
+    public ContactController(List<Contact> contacts) {
+        this.contacts = contacts;
+    }
 }
